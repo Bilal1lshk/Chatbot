74368bba295214ac4abbdc0cea3b9dabaa295380
bilalkamran <bilalsheikhsb77@gmail.com>
2026-09-05 17:16:57 +0500

    removed production errors

--- api.py
+++ api.py
@@ -15,8 +15,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        
+         "https://leadmanagmentsystem-nu.vercel.app",
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
